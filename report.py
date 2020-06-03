f6780fc0dfaa2299a15662f27297fe17c0333cbc
jadorex3 <dron9600@mail.ru>
2020-06-03 17:09:36 +0300

    update report.py

--- report.py
+++ report.py
@@ -20,39 +20,41 @@ with urllib.request.urlopen("https://json.medrating.org/todos") as url:
     TODOS = json.loads(url.read().decode())
 
 
+def three_dots(string, ln=50):
+    """Срезает строку до ln и стовит '...' """
+    string = f"{string[:ln]}...\n" if string[ln:] else f"{string}\n"
+    return string
+
+
 def todo_list(id_user):
-    """Формирует строку выполненных и невыполненых заданий пользователя"""
+    """Формирует строку выполненных и невыполненых заданий id_user пользователя"""
     completed = ''
     not_completed = ''
 
     for todo in TODOS:
         if id_user == todo.get('userId'):
             if todo['completed']:
-                if len(todo['title']) <= 50:
-                    completed += f"{todo['title']}\n"
-                else:
-                    completed += f"{todo['title'][:50]}...\n"
+                completed += three_dots(todo['title'])
             else:
-                if len(todo['title']) <= 50:
-                    not_completed += f"{todo['title']}\n"
-                else:
-                    not_completed += f"{todo['title'][:50]}...\n"
+                not_completed += three_dots(todo['title'])
     return completed, not_completed
 
 
 for user in USERS:
     COMPLETED, NOT_COMPLETED = todo_list(user.get('id'))
-
     if 'username' in user:
-        with open(f'tasks/{user["username"]}.txt', 'w', encoding='utf-8') as file:
+        PATH = f"tasks/{user['username']}.txt"
+
+    if os.path.exists(PATH):
+        pass  # Проверка существования файла
+    else:
+        with open(PATH, 'w', encoding='utf-8') as file:
             file.write(
-                f"""
-{user['name']} <{user["email"]}> {datetime.today().strftime('%d.%m.%Y %H:%M')}
-{user["company"]["name"]}
-
-Завершенные задачи:
-{COMPLETED}
-
-Оставшиеся задачи:
-{NOT_COMPLETED}
-                """)
+                f"{user['name']} <{user['email']}> {datetime.today().strftime('%d.%m.%Y %H:%M')}\n"
+                f"{user['company']['name']}\n"
+                f"\n"
+                f"Завершенные задачи:\n"
+                f"{COMPLETED}\n"
+                f"Оставшиеся задачи:\n"
+                f"{NOT_COMPLETED}\n"
+            )
