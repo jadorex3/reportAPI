bbcdcb308d210e7370949a225574cb4673861830
jadorex3 <50678049+jadorex3@users.noreply.github.com>
2020-06-03 20:43:02 +0300

    File existence check added report.py

--- report.py
+++ report.py
@@ -20,9 +20,9 @@ with urllib.request.urlopen("https://json.medrating.org/todos") as url:
     TODOS = json.loads(url.read().decode())
 
 
-def three_dots(string, ln=50):
-    """Срезает строку до ln и стовит '...' """
-    string = f"{string[:ln]}...\n" if string[ln:] else f"{string}\n"
+def three_dots(string, length=50):
+    """Срезает строку до ln и ставит '...' """
+    string = f"{string[:length]}...\n" if string[length:] else f"{string}\n"
     return string
 
 
@@ -32,7 +32,7 @@ def todo_list(id_user):
     not_completed = ''
 
     for todo in TODOS:
-        if id_user == todo.get('userId'):
+        if 'userId' in todo and id_user == todo.get('userId'):
             if todo['completed']:
                 completed += three_dots(todo['title'])
             else:
@@ -44,17 +44,20 @@ for user in USERS:
     COMPLETED, NOT_COMPLETED = todo_list(user.get('id'))
     if 'username' in user:
         PATH = f"tasks/{user['username']}.txt"
+    else:
+        continue
 
     if os.path.exists(PATH):
-        pass  # Проверка существования файла
-    else:
-        with open(PATH, 'w', encoding='utf-8') as file:
-            file.write(
-                f"{user['name']} <{user['email']}> {datetime.today().strftime('%d.%m.%Y %H:%M')}\n"
-                f"{user['company']['name']}\n"
-                f"\n"
-                f"Завершенные задачи:\n"
-                f"{COMPLETED}\n"
-                f"Оставшиеся задачи:\n"
-                f"{NOT_COMPLETED}\n"
-            )
+        created_time = datetime.fromtimestamp(os.stat(PATH).st_ctime)
+        os.rename(PATH, f"{PATH[:-4]}_{created_time.strftime('%Y-%m-%dT%H_%M')}.txt")
+
+    with open(PATH, 'w', encoding='utf-8') as file:
+        file.write(
+            f"{user['name']} <{user['email']}> {datetime.today().strftime('%d.%m.%Y %H:%M')}\n"
+            f"{user['company']['name']}\n"
+            f"\n"
+            f"Завершенные задачи:\n"
+            f"{COMPLETED}\n"
+            f"Оставшиеся задачи:\n"
+            f"{NOT_COMPLETED}\n"
+        )
